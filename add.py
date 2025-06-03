e84856779845bfd3368c821abec87517b7b8d3fe
polson6462 <polsonjoseph6@gmail.com>
2025-06-03 10:48:22 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,9 @@
+num1 = input("Enter the first number:  ")
+num2 = input("Enter the second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+sum = (num1 + num2)
+
+print("The sum of two number is: ", sum)
